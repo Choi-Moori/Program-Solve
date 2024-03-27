914105f7b8d2be6339658a74d6baf34ca0676bd5
Choi-Moori <90437809+Choi-Moori@users.noreply.github.com>
2024-03-27 14:35:07 +0900

    [Silver III] Title: 소수 구하기, Time: 784 ms, Memory: 38792 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,19 @@
+import sys
+M, N = map(int, sys.stdin.readline().split())
+
+prime = [False]*(N+1)
+
+prime[0] = prime[1] = True
+
+for i in range(2, N+1):
+    if prime[i] == False:
+        j = 2
+
+        while i*j <= N:
+            prime[i*j] = True
+            j += 1
+
+
+for i in range(M, N+1):
+    if not prime[i]:
+        print(i)
