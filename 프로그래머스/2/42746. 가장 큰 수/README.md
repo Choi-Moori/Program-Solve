d25ddbd8602b926ab871da19da247b66030431ef
Choi-Moori <90437809+Choi-Moori@users.noreply.github.com>
2024-06-05 09:31:14 +0900

    [level 2] Title: 가장 큰 수, Time: 1.57 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 77.2 MB, 시간: 1.91 ms
+메모리: 73.6 MB, 시간: 1.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 05일 09:29:51
+2024년 06월 05일 09:31:12
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -7,17 +7,13 @@ class Solution {
         for(int i = 0 ; i < numbers.length ; i++){
             str[i] = String.valueOf(numbers[i]);
         }
-        StringBuilder strbuild = new StringBuilder();
         
         Arrays.sort(str, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
 
         if(str[0].equals("0"))
             return "0";
             
-        for(String i : str){
-            strbuild.append(i);
-        }
         
-        return strbuild.toString();
+        return String.join("", str);
     }
 }
