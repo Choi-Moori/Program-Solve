7a7cf43032662877dd2006949df51423104088ea
Choi-Moori <90437809+Choi-Moori@users.noreply.github.com>
2024-06-10 17:25:12 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.04 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,28 +1,35 @@
 import java.util.*;
 class Solution {
-    public int[] solution(int[] numbers) {
-        
-        int[] answer1 = new int[numbers.length*numbers.length];
-        int num = 0;
-        Arrays.sort(numbers);
-        
+    public List<Integer>/*int[]*/ solution(int[] numbers) {
+        List<Integer> answerlist = new ArrayList<>();
         for(int i = 0 ; i < numbers.length ; i++){
-            for (int j = i+1 ; j < numbers.length ; j++){
-                answer1[num] = numbers[i] + numbers[j];
-                num++;
+            for(int j = i+1 ; j < numbers.length ; j++){
+                int num = numbers[i] + numbers[j];
+                if(!answerlist.contains(num))
+                    answerlist.add(num);
             }
         }
-        answer1 = Arrays.copyOf(answer1, num);
-        Arrays.sort(answer1);
-        num = 0;
-        int[] answer = new int[answer1.length];
-        for(int i = 0 ; i < answer.length ; i++){
-            if(i == 0 || answer1[i] != answer1[i-1]){
-                answer[num++] = answer1[i];
-            }
-        }
-        answer = Arrays.copyOf(answer, num);
-        System.out.println(Arrays.toString(answer));
-        return answer;
+        Collections.sort(answerlist);
+        System.out.println(answerlist);
+//         int[] answer1 = new int[numbers.length*numbers.length];
+//         int num = 0;
+        
+//         for(int i = 0 ; i < numbers.length ; i++){
+//             for (int j = i+1 ; j < numbers.length ; j++){
+//                 answer1[num++] = numbers[i] + numbers[j];
+//             }
+//         }
+        
+//         answer1 = Arrays.copyOf(answer1, num);
+//         Arrays.sort(answer1);
+//         num = 0;
+//         int[] answer = new int[answer1.length];
+//         for(int i = 0 ; i < answer.length ; i++){
+//             if(i == 0 || answer1[i] != answer1[i-1]){
+//                 answer[num++] = answer1[i];
+//             }
+//         }
+        
+        return answerlist;
     }
 }
