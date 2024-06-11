a9e5f84408cda88a728c01b4e646c4d9b79bbc02
Choi-Moori <90437809+Choi-Moori@users.noreply.github.com>
2024-06-11 09:34:46 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 220 ms, Memory: 18028 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
@@ -7,8 +7,7 @@ public class Main {
         int num = sc.nextInt();
 
         ArrayList<String> st = new ArrayList<>();
-        int state = 0;
-        String result = "";
+        StringBuilder result = new StringBuilder();
         if(num == 1){
             System.out.println(sc.next());
         }
@@ -16,20 +15,15 @@ public class Main {
             for(int i = 0 ; i < num ; i++){
                 st.add(sc.next());
             }
-            result = st.get(0);
+            result.append(st.get(0));
             for(int i = 1 ; i < num ; i++){
                 for(int j = 0 ; j < result.length() ; j++){
-                    if(result.substring(j,j+1).equals(st.get(i).substring(j,j+1))){
-                        continue;
-                    }
-                    else{
-                        result = result.substring(0,j) + "?"+result.substring(j+1);
+                    if(!(result.substring(j,j+1).equals(st.get(i).substring(j,j+1)))){
+                        result.replace(j,j+1,"?");
                     }
                 }
             }
             System.out.println(result);
         }
-
-
     }
 }
