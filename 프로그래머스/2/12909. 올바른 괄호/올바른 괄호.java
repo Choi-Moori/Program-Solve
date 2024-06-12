dd09d656a7eb1ccf476cbe054c534a203aab9e50
Choi-Moori <90437809+Choi-Moori@users.noreply.github.com>
2024-06-12 10:46:55 +0900

    [level 2] Title: 올바른 괄호, Time: 53.84 ms, Memory: 58.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,19 +2,13 @@ class Solution {
     boolean solution(String s) {
         boolean answer = true;
         String[] st = s.split("");
-        int i = 0;
-        for(String str : st) {
-            if(str.equals("("))
-                i++;
-            else
-                i--;
-
-            if (i < 0){
-                return false;
-            }
+        int i = 0 ;
+        for(String str : st){
+            if(str.equals("(")) i++;
+            else i--;
+            if(i < 0) return false;
         }
         
-        
-        return i > 0 ? false : answer;
+        return i == 0 ? answer : false;
     }
 }
