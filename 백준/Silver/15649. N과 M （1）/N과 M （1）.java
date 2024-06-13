323932887025920c3cd0971498b44b241632c3e3
Choi-Moori <90437809+Choi-Moori@users.noreply.github.com>
2024-06-13 19:22:45 +0900

    [Silver III] Title: N과 M (1), Time: 2044 ms, Memory: 59592 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    static boolean[] visit;
+    static int[] list;
+    static List<Integer> num;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String str = bf.readLine();
+        num = new ArrayList<>();
+
+        for(String st : str.split(" ")){
+            num.add(Integer.parseInt(st));
+        }
+        visit = new boolean[num.get(0)+1];
+        list = new int[num.get(1)];
+
+        permutation(0);
+    }
+
+    static void permutation(int count){
+        if(count == num.get(1)){
+            for(int i : list){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for(int i = 1 ; i <= num.get(0) ; i++){
+            if(!visit[i]){
+                visit[i] = true;
+                list[count] = i;
+                permutation(count+1);
+                visit[i] = false;
+            }
+        }
+    }
+
+}
