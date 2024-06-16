66aa1c74c7920121dd3440481fed5e617052755a
Choi-Moori <90437809+Choi-Moori@users.noreply.github.com>
2024-06-16 18:57:37 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 128 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -1,29 +1,39 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
+
         long[][] dp = new long[n][10];
-        for (int i = 0; i < 10; ) {
-            dp[0][i++] = 1;
-        }
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < 10; j++) {
-                if (j == 0)
-                    dp[i][j] = dp[i - 1][j + 1] % 1000000000;
-                else if (j == 9)
-                    dp[i][j] = dp[i - 1][j - 1] % 1000000000;
-                else
-                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % 1000000000;
+        if(n == 0)
+            System.out.println(0);
+        else {
+            for (int i = 0; i < 10; i++) {
+                dp[0][i] = 1;
             }
-        }
-        long res = 0;
-        for (int i = 0; i < 10; i++) {
-            res += dp[n - 1][i];
-        }
-        System.out.println((res - dp[n - 1][0]) % 1000000000);
+            for (int i = 1; i < n; i++) {
+                for (int j = 0; j < 10; j++) {
+                    if (j == 0) {
+                        dp[i][j] = dp[i - 1][j + 1] % 1000000000;
+                    } else if (j == 9) {
+                        dp[i][j] = dp[i - 1][j - 1] % 1000000000;
+                    } else {
+                        dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % 1000000000;
+                    }
+                }
+            }
+            long total = 0;
+            for (long i : dp[n - 1]) {
+                total += i;
+            }
+            total -= dp[n - 1][0];
 
+            System.out.println(total%1000000000);
+        }
     }
 }
