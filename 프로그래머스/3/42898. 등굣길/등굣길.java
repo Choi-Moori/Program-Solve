f2ae9881efc8a16b987fc58661c67dafe4fdf2fb
Choi-Moori <90437809+Choi-Moori@users.noreply.github.com>
2024-06-27 09:23:09 +0900

    [level 3] Title: 등굣길, Time: 0.60 ms, Memory: 52 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -2,29 +2,38 @@ class Solution {
     public int solution(int m, int n, int[][] puddles) {
         int[][] res = new int[n][m];
         boolean end = true;
-        if(puddles[0].length > 0){
-            for(int x[] : puddles){
-                res[x[1]-1][x[0]-1] = -1;
-            }
+        for(int[] i : puddles)
+            res[i[1]-1][i[0]-1] = -1;
+        for(int i = 1 ; i < m ; i++){
+            if(res[0][i] == -1) end=false;
+            if(end) res[0][i] = 1;
+            else res[0][i] = -1;
         }
-        for(int i = 1 ;i < n; i++){
-            res[i][0] = (res[i][0] == -1 | res[i-1][0] == -1) ? -1 : 1;
-        }
-        for(int j = 1 ;j < m ;j++){
-            res[0][j] = (res[0][j] == -1 | res[0][j-1] == -1) ? -1 : 1;
+        end = true;
+        for(int i = 1 ; i < n ; i++){
+            if(res[i][0] == -1) end=false;
+            if(end) res[i][0] = 1;
+            else res[i][0] = -1;
         }
 
+        res[0][0] =1;
+
         for(int i = 1; i < n ; i++){
-            for(int j = 1 ; j < m ; j++){
+            for(int j = 1 ; j < m;j++ ){
                 if(res[i][j] != -1){
-                    int a = res[i-1][j] == -1 ? 0 : res[i-1][j];
-                    int b = res[i][j-1] == -1 ? 0 : res[i][j-1];
-                    res[i][j] = (a+b) == 0 ? -1 : (a+b) % 1000000007;
+                    if(res[i-1][j] == -1){
+                        res[i][j] = res[i][j-1];
+                    }
+                    else if(res[i][j-1] == -1){
+                        res[i][j] = res[i-1][j];
+                    }
+                    else{
+                        res[i][j] = (res[i-1][j] + res[i][j-1])%1000000007;
+                    }
                 }
             }
         }
 
-        return res[n-1][m-1] == -1? 0 : res[n-1][m-1];
-
+        return Math.max((res[n - 1][m - 1]), 0);
     }
 }
