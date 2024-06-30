579a905c26f2f12d41125547a0297e5ade509980
Choi-Moori <90437809+Choi-Moori@users.noreply.github.com>
2024-06-30 17:29:32 +0900

    [Silver II] Title: DFS와 BFS, Time: 292 ms, Memory: 20996 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static boolean[] visitdfs;
+    static boolean[] visitbfs;
+    static List<List<Integer>> list;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] st = br.readLine().split(" ");
+        list = new ArrayList<>();
+        int n = Integer.parseInt(st[0]);
+        int m = Integer.parseInt(st[1]);
+        int target = Integer.parseInt(st[2]);
+        visitdfs = new boolean[n+1];
+        visitbfs = new boolean[n+1];
+
+        for(int i = 0; i <= n ; i++){
+            list.add(new ArrayList<>());
+        }
+
+        for(int i = 1 ; i <= m ; i++){
+            String[] str = br.readLine().split(" ");
+            int a = Integer.parseInt(str[0]);
+            int b = Integer.parseInt(str[1]);
+            list.get(a).add(b);
+            list.get(b).add(a);
+        }
+
+        for(int i = 1 ; i <= n ; i++){
+            Collections.sort(list.get(i));
+        }
+
+        dfs(target);
+        System.out.println();
+        bfs(target);
+    }
+
+    public static void dfs(int start){
+        System.out.print(start + " ");
+        visitdfs[start] = true;
+        for(int i : list.get(start)){
+            if(!visitdfs[i])
+                dfs(i);
+        }
+    }
+    public static void bfs(int start){
+        Queue<List<Integer>> queue = new LinkedList<>();
+        queue.add(list.get(start));
+        while(!queue.isEmpty()){
+            List<Integer> temp =queue.poll();
+            visitbfs[start] = true;
+            for(int i : temp){
+                if(!visitbfs[i]) {
+                    queue.add(list.get(i));
+                    System.out.print(start + " ");
+                    start = i;
+                    visitbfs[i] = true;
+                }
+            }
+        }
+        System.out.println(start);
+    }
+}
