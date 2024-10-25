b16c49af7867bac59fe3da7eb533b76b0c45bd0c
Choi-Moori <90437809+Choi-Moori@users.noreply.github.com>
2024-10-25 21:41:58 +0900

    [Gold V] Title: 빗물, Time: 108 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -1,43 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        List<int[]> temp = new ArrayList<>();
+
         String[] st = br.readLine().split(" ");
 
-        int height = Integer.parseInt(st[0]);
-        int width = Integer.parseInt(st[1]);
-        int[] board = new int[width];
-
-        String[] blocks = br.readLine().split(" ");
-        for (int i = 0; i < width; i++) {
-            board[i] = Integer.parseInt(blocks[i]);
-        }
-
+        int x = Integer.parseInt(st[0]);
+        int y = Integer.parseInt(st[1]);
         int result = 0;
-        int[] leftMax = new int[width];
-        int[] rightMax = new int[width];
 
-        leftMax[0] = board[0];
-        for (int i = 1; i < width; i++) {
-            leftMax[i] = Math.max(leftMax[i - 1], board[i]);
+        int[] board = new int[y];
+        int i = 0;
+        for(String str : br.readLine().split(" ")){
+            board[i++] = Integer.parseInt(str);
         }
 
-        rightMax[width - 1] = board[width - 1];
-        for (int i = width - 2; i >= 0; i--) {
-            rightMax[i] = Math.max(rightMax[i + 1], board[i]);
+        int[] left = new int[y];
+        int[] right = new int[y];
+        int[] res = new int[y];
+        left[0] = board[0];
+        for (i = 1 ; i < y ; i++){
+            left[i] = Math.max(left[i-1], board[i]);
+        }
+        right[y-1] = board[y-1];
+        for(i = y-2 ; i >= 0 ; i--){
+            right[i] = Math.max(right[i+1], board[i]);
         }
 
-        for (int i = 0; i < width; i++) {
-            int minHeight = Math.min(leftMax[i], rightMax[i]);
-            if (minHeight > board[i]) {
-                result += minHeight - board[i];
-            }
+        for(i = 0 ; i < y ; i++){
+            result += Math.min(left[i], right[i]) - board[i];
         }
-
         System.out.println(result);
     }
 }
