3a00e31e4078c6dde4e0f31112cb38911c7854b5
Choi-Moori <90437809+Choi-Moori@users.noreply.github.com>
2025-01-11 20:03:12 +0900

    [level 2] Title: 기능개발, Time: 0.41 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,31 +1,35 @@
 import java.util.*;
 
 class Solution {
-    public Object[] solution(int[] progresses, int[] speeds) {
-        Deque<Integer> deq = new ArrayDeque<>();
-        List<Integer> result = new ArrayList<>();
-
-        for (int i=0; i<progresses.length; i++) {
-            int temp = (100 - progresses[i]) / speeds[i];
-            if ((100 - progresses[i]) % speeds[i] != 0 )
-                temp += 1;
-            deq.offer(temp);
+    public int[] solution(int[] progresses, int[] speeds) {
+        List<Integer> list = new ArrayList<>();
+        
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0 ; i < speeds.length; i++){
+            queue.offer((int) Math.ceil((100.0 - progresses[i]) / speeds[i]));
         }
-
-        int day = 0;
-        while(!deq.isEmpty()) {
-            day = deq.pollFirst();
-            int count = 1;
-            while (!deq.isEmpty() && day >= deq.peekFirst()) {
-                deq.pollFirst();
+        
+        int count = 1;
+        int temp = queue.poll();
+        while(!queue.isEmpty()){
+            if(!queue.isEmpty() && temp >= queue.peek()){
                 count++;
-                if (deq.isEmpty()) {
-                    break;
-                }
+                queue.poll();
             }
-            result.add(count);
+            else{
+                list.add(count);
+                count = 1;
+                temp = queue.poll();
+            }
         }
-
-        return result.toArray();
+        list.add(count);
+        
+        int[] answer = new int[list.size()];
+        
+        for(int i = 0 ; i < list.size(); i++){
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
     }
 }
